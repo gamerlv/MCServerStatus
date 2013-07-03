a3da17a47b7c93302221fac5d0632e74755626eb
Levi Voorintholt <levi.voorintholt+git@gmail.com>
2013-07-03 21:28:30 +0200

    Update voor 1.6.1

--- MCServerStatus/Minecraft/Stats.php
+++ MCServerStatus/Minecraft/Stats.php
@@ -12,7 +12,7 @@ class Stats {
 			throw new StatsException("Could not connect to the Minecraft server.");
 		}
 
-		fwrite($socket, "\xfe\x01");
+		fwrite($socket, "\xfe\x01\xfa");
     	$data = fread($socket, 1024); 
 		fclose($socket);
 
@@ -24,12 +24,59 @@ class Stats {
 			return $stats;
 		}
 
-		$data = substr($data, 9);
+		$data = substr($data, 1);
 		$data = mb_convert_encoding($data, 'auto', 'UCS-2');
-		$data = explode("\x00", $data);
+
+//Code from mc-creative.nl status checker
+
+		//Split into array
+		$z = explode("\xA7", $data);
+		$d = array('');
+
+		//This may confuse you, ANONYMOUS FUNCTION FTW
+		$colorIt = function($str, $colorCode){
+			$colors = array(
+				'0'=>'#000000',
+				'1'=>'#0000AA',
+				'2'=>'#00AA00',
+				'3'=>'#00AA00',
+				'4'=>'#AA0000',
+				'5'=>'#AA00AA',
+				'6'=>'#FFAA00',
+				'7'=>'#AAAAAA',
+				'8'=>'#555555',
+				'9'=>'#5555FF',
+				'a'=>'#55FF55',
+				'b'=>'#55FFFF',
+				'c'=>'#FF5555',
+				'd'=>'#FF55FF',
+				'e'=>'#FFFF55',
+				'f'=>'#FFFFFF',
+			);
+			if ( !isset($colors[$colorCode]) ) return $str;//Return unmodified string if colo not found
+
+			return '<span style="color:'.$colors[$colorCode].'">'.$str.'</span>';
+		};
+		
+		$total = count($z)-3;
+		foreach($z as $i => $v) {
+			if ( $i == 0 ) { $d[0] .= $v; continue; } //Add it then skip the rest
+			if ( $i <= $total ){
+				$colorCode = substr($v,0,1);
+				$d[0] .= $colorIt(substr($v,1), $colorCode );
+			} else {
+				$d[] = $v;
+			}
+		}
+		$data = $d;
+
+		//FIXME: this is part of the protcol spec, get it from $data, not hardcoded
+		$stats->protocol_version = 74; //1.6
+		$stats->game_version= "1.6";
+
 
 		$stats->is_online = true;
-		list($stats->protocol_version, $stats->game_version, $stats->motd, $stats->online_players, $stats->max_players) = $data;
+		list( $stats->motd, $stats->online_players, $stats->max_players) = $data;
 
 		return $stats;
 
